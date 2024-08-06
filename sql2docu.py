7e4b4103675d579329cf4008c24d976aa19fe897
fly-pigs31 <2316735315@qq.com>
2024-08-06 08:31:33 +0800

    修改使用方法

--- sql2docu.py
+++ sql2docu.py
@@ -6,115 +6,10 @@ from openpyxl import Workbook
 from openpyxl.styles import Font, Alignment, Border, Side, PatternFill
 from pgsql.mypgsql import schema_info_s, Conn
 
-conn = Conn("data", "postgres", "PPJbh9z7zrRrH9a4", "192.168.2.250", "5432", "xhz")
-# conn = Conn("data", "postgres", "PPJbh9z7zrRrH9a4", "192.168.2.250", "5432", "mjxy")
-# conn2 = Conn("byshx", "postgres", "PPJbh9z7zrRrH9a4", "192.168.2.248", "5432", "adm")
+conn = Conn("data", "postgres", "pwd", "192.168.1.1", "5432", "xhz")
+
 # 输入表名
-input_string = '''edu_app_jxgk_gkszrt
-edu_app_jxgk_gxyzs
-edu_app_jxgk_jccg
-edu_app_jxgk_jpzxkc
-edu_app_jxgk_jxzl
-edu_app_jxgk_kcjsqk
-edu_app_jxgk_kcsz
-edu_app_jxgk_sj_jxnldsqk
-edu_app_jxgk_syfx
-edu_app_jxgk_syjg
-edu_app_jxgk_xjydqk
-edu_app_jxgk_xnfz_jyjg
-edu_app_jxgk_xsjshjqk
-edu_app_jxgk_xsskqk
-edu_app_jxgk_zsbksqk
-edu_app_jxgk_zsgm
-edu_app_jxgk_zsgmsj
-edu_app_jxgk_zsgmzj
-edu_app_jxgk_zygmpm
-edu_app_jxgk_zyjs
-edu_app_jxgk_zyjsfw
-edu_app_jxgk_zyjsqk
-edu_app_jxgk_zykcg
-edu_app_jxgk_zzyfx
-edu_app_kygk_bzyzcg
-edu_app_kygk_gl
-edu_app_kygk_hxkyxm
-edu_app_kygk_hxkyxm_gl
-edu_app_kygk_kycgj
-edu_app_kygk_kycgj_nf
-edu_app_kygk_kypt
-edu_app_kygk_xqhz
-edu_app_kygk_zscqsq
-edu_app_kygk_zscqzr
-edu_app_kygk_zxkyxm
-edu_app_kygk_zxkyxm_gl
-edu_app_rsgk_cbpx
-edu_app_rsgk_fxjx
-edu_app_rsgk_gccrc
-edu_app_rsgk_gl
-edu_app_rsgk_gzdl
-edu_app_rsgk_hydsfx
-edu_app_rsgk_jsgk
-edu_app_rsgk_jzgrs_glry
-edu_app_rsgk_jzgrs_zrjs
-edu_app_rsgk_jzgzcfb
-edu_app_rsgk_jzgzs
-edu_app_rsgk_nlfb
-edu_app_rsgk_ryfb
-edu_app_rsgk_ssb
-edu_app_rsgk_szdwcg
-edu_app_rsgk_txjzgrs_glry
-edu_app_rsgk_txjzgrs_zrjs
-edu_app_rsgk_xlfb
-edu_app_rsgk_xwjsrsbl
-edu_app_rsgk_zcdjfb
-edu_app_rsgk_zcfb_jf
-edu_app_rsgk_zcfb_js
-edu_app_rsgk_zrjs
-edu_app_rsgk_zrjscjpx
-edu_app_rsgk_zrjsqysj
-edu_app_rsgk_ztqk
-edu_app_rsgk_zzmmfb
-edu_app_sygk_byjyqk
-edu_app_sygk_cjrh
-edu_app_sygk_gjyx
-edu_app_sygk_gl
-edu_app_sygk_jyjg
-edu_app_sygk_kyfw
-edu_app_sygk_rcpy
-edu_app_sygk_ssgk
-edu_app_sygk_xxgk
-edu_app_sygk_zykc
-edu_app_xsgk_byqxqk
-edu_app_xsgk_dekt
-edu_app_xsgk_fwxszl
-edu_app_xsgk_gfjygx
-edu_app_xsgk_gl
-edu_app_xsgk_jzcg
-edu_app_xsgk_qjgk
-edu_app_xsgk_qjgk_dqqj
-edu_app_xsgk_stgm
-edu_app_xsgk_xshjgk
-edu_app_xsgk_xsjshj
-edu_app_xsgk_xsjshj_dict
-edu_app_xsgk_zxs_mzfb
-edu_app_xsgk_zxs_sydfb
-edu_app_xsgk_zxs_zzmmfb
-edu_app_xsgk_zxsgk_yx
-edu_app_xsgk_zxsmzfb
-edu_app_xsgk_zxsxbfb
-edu_app_zhxq_jkygk
-edu_app_zhxq_jxgk
-edu_app_zhxq_kygk
-edu_app_zhxq_kygk_kycg
-edu_app_zhxq_rsgk
-edu_app_zhxq_xsgk
-edu_app_zztsgk_djdzs
-edu_app_zztsgk_gcqk_kjgqk
-edu_app_zztsgk_gcqk_zl
-edu_app_zztsgk_jhdt
-edu_app_zztsgk_jyph_dzjy
-edu_app_zztsgk_jyph_sjjy
-edu_app_zztsgk_tsflqk
-edu_app_zztsgk_xbjyqk
+input_string = '''
 '''
 
 tables = input_string.strip().split('\n')
@@ -247,7 +142,7 @@ for item in result:
         print(f"row:{row}")
     # print(line)
 print("执行写入函数")
-filename = "西杭职数据库文档"
+filename = "excel_name"
 docx_name = f"{filename}.docx"
 xlsx_name = f"{filename}.xlsx"
 write_to_excel(result, xlsx_name)
