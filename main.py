04a2440db702e5e33c15995ff9c9d7a411536c81
fly-pigs31 <2316735315@qq.com>
2024-08-06 08:40:38 +0800

    在main.py中使用

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
-from pgsql import mymysql
-from pgsql.mymysql import MySQLConn
-from xgui.write_result import write_to_word
+from pgsql.mypgsql import schema_info_s, Conn
+# from pgsql.mymysql import schema_info_s, MySQLConn
+from sql2docu import write_to_excel
+from sql2word import write_to_word
 
-"""
-write_to_word
-"""
-input_tables = '''
-db_user
+conn = Conn("data", "postgres", "pwd", "192.168.1.1", "5432", "xhz")
+# MySQLConn("data", "root", "pwd", "192.168.1.1", "3306", "xhz")
+# 输入表名
+input_string = '''
+
 '''
-tables = input_tables.strip().split('\n')
-conn = MySQLConn("db_name", "root", "root", "localhost", 3306)
-result = mymysql.schema_info_s(tables, conn)
-doc_path = "result/doc/tayipingyang.docx"
-table_header_rows = ["字段名", "数据类型", "是否为空", "默认值", "注释"]
-# xlsx_path = "/result/doc/result.xlsx"
-write_to_word(result, doc_path, table_header_rows)
+
+# 清除字符串中的空格和换行符，并按换行符分割成列表
+tables = input_string.strip().split('\n')
+
+# 使用集合去重，再转换为列表
+tables = list(set(tables))
+result = schema_info_s(tables, conn)
+for item in result:
+    for row in item:
+        print(f"row:{row}")
+    # print(line)
+print("执行写入函数")
+filename = "excel_name"
+docx_name = f"{filename}.docx"
+xlsx_name = f"{filename}.xlsx"
+# 导出到excel
+write_to_excel(result, xlsx_name)
+# 导出到word
+# write_to_word(result, docx_name)
